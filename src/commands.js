1083d72953ed4f046576ceddbe323086c35564b5
d.nayezzhy <d.nayezzhy@gmail.com>
2025-05-03 10:48:53 +0300

    feat: commands module

---
+++ src/commands.js
@@ -0,0 +1,9 @@
+import { hash } from './hash.js';
+import { compress } from './compress.js';
+import { decompress } from './compress.js';
+
+export const commands = {
+  hash,
+  compress,
+  decompress
+};
