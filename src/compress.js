c8544665d5c71ffad1fe96ce831689f9b15d65ff
d.nayezzhy <d.nayezzhy@gmail.com>
2025-05-03 10:12:42 +0300

    feat: implemented compress

---
+++ src/compress.js
@@ -0,0 +1,29 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createBrotliCompress, createBrotliDecompress } from 'zlib';
+import { pipeline } from 'stream/promises';
+
+export const compress = async (sourcePath, destinationPath) => {
+  try {
+    const readStream = createReadStream(sourcePath);
+    const writeStream = createWriteStream(destinationPath);
+    const compressStream = createBrotliCompress();
+    
+    await pipeline(readStream, compressStream, writeStream);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const decompress = async (sourcePath, destinationPath) => {
+  try {
+    const readStream = createReadStream(sourcePath);
+    const writeStream = createWriteStream(destinationPath);
+    const decompressStream = createBrotliDecompress();
+    
+    await pipeline(readStream, decompressStream, writeStream);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
