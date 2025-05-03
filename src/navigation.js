54fdd8e5cc032de5a376de29e26c00db8fab35a6
d.nayezzhy <d.nayezzhy@gmail.com>
2025-05-03 12:19:27 +0300

    feat: implement ls

--- src/navigation.js
+++ src/navigation.js
@@ -1,4 +1,6 @@
 import { chdir, cwd } from 'process';
+import { readdir } from 'fs/promises';
+import { stat } from 'fs/promises';
 import { join } from 'path';
 
 import { NODE_ENV } from './env';
@@ -22,6 +24,39 @@ export const up = async () => {
 export const cd = async (path) => {
   try {
     await chdir(path);
+    return true;
+  } catch (error) {
+    if (isDev) console.error(error);
+    return false;
+  }
+};
+
+export const ls = async () => {
+  try {
+    const items = await readdir(cwd());
+    const itemsWithStats = await Promise.all(
+      items.map(async (item) => {
+        const stats = await stat(join(cwd(), item));
+        return {
+          name: item,
+          isDirectory: stats.isDirectory()
+        };
+      })
+    );
+
+    itemsWithStats.sort((a, b) => {
+      if (a.isDirectory === b.isDirectory) {
+        return a.name.localeCompare(b.name);
+      }
+      return a.isDirectory ? -1 : 1;
+    });
+
+    console.log('\nCurrent directory content:');
+    console.log('Type\t\tName');
+    console.log('----------------------------');
+    itemsWithStats.forEach(item => {
+      console.log(`${item.isDirectory ? 'Directory' : 'File'}\t\t${item.name}`);
+    });
     return true;
   } catch (error) {
     if (isDev) console.error(error);
